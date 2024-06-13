90dca86686017c68730825b25f2ca7a38910fc85
MeenaMisae <meenahiwatashi@gmail.com>
2024-06-13 20:40:53 -0300

    🔧 chore: php docs do ide helper no attribute category

--- app/Models/AttributeCategory.php
+++ app/Models/AttributeCategory.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property int $attribute_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory whereAttributeId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|AttributeCategory whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class AttributeCategory extends Model
 {
     use HasFactory;
