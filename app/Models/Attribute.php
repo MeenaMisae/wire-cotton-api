d4ae541d44bfc6114af328a89ee85010289ac1bb
MeenaMisae <meenahiwatashi@gmail.com>
2024-06-13 20:40:25 -0300

    ✨ feat: model de atributos

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -4,9 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\AttributeOption> $attributeOptions
+ * @property-read int|null $attribute_options_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Attribute whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Attribute extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
+
+    public function attributeOptions(): HasMany
+    {
+        return $this->hasMany(AttributeOption::class, 'attribute_id');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(AttributeCategory::class, 'attribute_category');
+    }
 }
