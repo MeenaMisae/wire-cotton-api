940b8f0f365c299b701990f2b7d8963a2b24ef6b
MeenaMisae <meenahiwatashi@gmail.com>
2024-06-13 20:41:51 -0300

    🔧 chore: php docs do ide helper no model sku

--- app/Models/Sku.php
+++ app/Models/Sku.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $product_id
+ * @property string $code
+ * @property string $price
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku whereProductId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Sku whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Sku extends Model
 {
     use HasFactory;
