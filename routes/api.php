2ed644e42a2d6d3c446f7487aeb69d07eb38c25c
MeenaMisae <meenahiwatashi@gmail.com>
2024-06-18 14:37:50 -0300

    ✨ feat: rota de criação de produtos

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Product\Attribute\AttributeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Product\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +13,5 @@ Route::get('/user', function (Request $request) {
 Route::prefix('products')->group(static function () {
     Route::get('categories', [CategoryController::class, 'index']);
     Route::get('attributes/{categoryId}', [AttributeController::class, 'getCategoryAttributes']);
+    Route::post('', [ProductController::class, 'store']);
 });
