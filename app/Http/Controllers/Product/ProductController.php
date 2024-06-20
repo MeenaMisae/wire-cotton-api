c9b0af287c4ef9328191b4c2db6367536fb53b01
MeenaMisae <meenahiwatashi@gmail.com>
2024-06-19 22:06:40 -0300

    🩹 fix: tipando retorno da função

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Product;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
         return response()->json(['request' => $request->all()]);
     }
